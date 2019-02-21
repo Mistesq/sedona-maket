ff03d03a8005342516fa4f6df88c97a47d9f9784
Alex Shapovalov <lancelotxz@gmail.com>
2019-02-22 00:35:43 +0200

    адаптивность внутренней страницы, фикс бага анимации, опечаток в коде

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ var buttonForm = document.querySelector(".search-form-button");
 var form = document.querySelector(".form-search");
 var formDate = form.querySelector(".search-arrival-date");
 
+function focus() {
+  formDate.focus();
+  console.log('9 rabotau');
+}
+
 buttonForm.addEventListener("click", function functionName(evt) {
   evt.preventDefault();
   form.classList.toggle("modal-show");
-  formDate.focus();
+  setTimeout(focus, 2000);
 } );
